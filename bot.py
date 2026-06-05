b12c4ff4b6fc37836204e0d4c938a82276eb7918
mhdyt628-ctrl <mhdyt628@gmail.com>
2026-06-05 13:41:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -224,7 +224,6 @@ def main():
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
     logger.info("ربات شروع به کار کرد...")
-    app.run_polling(drop_pending_updates=True)
-
+    app.run_polling(drop_pending_updates=True, close_loop=False)
 if __name__ == "__main__":
     main()
